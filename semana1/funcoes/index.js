dfa707a98964eefdb30f0487de3d22f088141cc0
matheus1810 <matheussouza1810@gmail.com>
2021-06-17 16:28:08 -0300

    atividades dia 17/16/21

---
+++ semana1/funcoes/index.js
@@ -0,0 +1,76 @@
+//-------------------------------------------------------//
+//-------------------interpretação de código-------------//
+//-------------------------------------------------------//
+
+//======================== EX 1 =========================//
+//--A)O que vai ser impresso no console?
+//--R:10 e 50
+
+//--B)O que aconteceria se retirasse os dois console.log e simplesmente invocasse a função minhaFuncao(2) e minhaFuncao(10)? O que apareceria no console?
+//--R:nada vai aparecer porque eu nao pedi para aparecer o resultado com o console log
+
+//======================== EX 2 =========================//
+
+//--A)Explique o que essa função faz e qual é sua utilidade?
+//--R:ela pede uma frase para o usuario e transforma todas as palavras para minúsculas isso deve servir para pesquisarmos uma palavra específica dentro de um texto
+
+//--B)Determine qual será a saída no console para cada uma das 3 entradas do usuário:
+//true
+//true
+//true
+
+//--------------------------------------------------------//
+//-----------------------escrita de código----------------//
+//--------------------------------------------------------//
+
+//========================= EX 1 =========================//
+
+/*nomeUser = ("matheus")
+idadeUser = Number(21)
+cidadeUser = ("fraiburgo")
+profissaoUser = ("web developer")
+function sobre(nome,idade,cidade,profissao){
+    const dados = (`meu nome é ${nome} tenho ${idade} de idade, moro em ${cidade}e trabalho como ${profissao}.`)
+    return dados
+
+
+}
+ const resultado = sobre(nomeUser,idadeUser,cidadeUser,profissaoUser)
+console.log(resultado)*/
+
+//============================ EX 2 ======================//
+
+/*function numeros(n1,n2){
+    soma = n1 + n2 
+    return soma
+}
+ resultado = numeros (2,2)
+ console.log(soma)*/
+
+/*function numeros(n1,n2){
+    maior = n1 >= n2 
+    return maior
+}
+ resultado = numeros (2,2)
+ console.log(maior)*/
+
+ /*function numeros(numero){
+    par = numero % 2 ===0
+    return par
+}
+ resultado = numeros (57)
+ console.log(par)*/
+
+
+/*const texto = ("testando código")
+
+ function mensagem(msg){
+     resultado = msg.length
+     resultado = msg.toUpperCase()
+    return resultado
+   
+}
+resto = mensagem(texto)
+console.log(resultado)*/
+
+ 
