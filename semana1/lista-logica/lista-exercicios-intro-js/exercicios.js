bb68a32aed4bf6fdc7ea1dc7191a7df3d4bbdb26
matheus1810 <matheussouza1810@gmail.com>
2021-06-18 16:40:30 -0300

    vou terminar depois os exercicios

---
+++ semana1/lista-logica/lista-exercicios-intro-js/exercicios.js
@@ -0,0 +1,121 @@
+// EXEMPLOS DE IMPLEMENTAÇÃO ---------------------------------------------------------------
+
+// EXERCÍCIO 0A
+function soma(num1, num2) {
+  return num1 + num2
+}
+soma(1,2)
+
+// EXERCÍCIO 0B
+function imprimeMensagem() {
+  const mensagem = prompt('Digite uma mensagem!')
+
+  console.log(mensagem)
+}
+
+// EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
+
+// EXERCÍCIO 01
+
+/*alturaUser = 3
+larguraUser =5
+
+function calculaAreaRetangulo(altura,largura) {
+    retangulo = altura * largura
+    console.log(retangulo)
+   return retangulo
+   
+}
+mostra = calculaAreaRetangulo(alturaUser,larguraUser)
+console.log(mostra)*/
+
+
+// EXERCÍCIO 02
+
+anoAtual = prompt("qual ano atual?")
+idadeDaPessoa= prompt("em que ano voce nasceu?")
+function imprimeIdade(anoN,anoA) {
+  let idade = anoN - anoA
+  return idade
+
+}
+imprimeIdade(anoAtual,idadeDaPessoa)
+
+// EXERCÍCIO 03
+function calculaIMC(peso, altura) {
+  // implemente sua lógica aqui
+   
+}
+
+// EXERCÍCIO 04
+function imprimeInformacoesUsuario() {
+  // implemente sua lógica aqui
+  // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
+
+}
+
+// EXERCÍCIO 05
+function imprimeTresCoresFavoritas() {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 06
+function retornaStringEmMaiuscula(string) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 07
+function calculaIngressosEspetaculo(custo, valorIngresso) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 08
+function checaStringsMesmoTamanho(string1, string2) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 09
+function retornaPrimeiroElemento(array) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 10
+function retornaUltimoElemento(array) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 11
+function trocaPrimeiroEUltimo(array) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 12
+function checaIgualdadeDesconsiderandoCase(string1, string2) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 13
+function checaRenovacaoRG() {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 14
+function checaAnoBissexto(ano) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 15
+function checaValidadeInscricaoLabenu() {
+  // implemente sua lógica aqui
+
+}
