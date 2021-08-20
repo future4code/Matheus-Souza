bc8fc61b74697d7312ae2f3518f967bef69cd511
matheus1810 <matheussouza1810@gmail.com>
2021-08-20 16:09:47 -0300

    estilização inicial Aplication Form Page

--- labex/src/pages/ApplicationFormPage.js
+++ labex/src/pages/ApplicationFormPage.js
@@ -1,5 +1,25 @@
 import React from "react"
 import { useHistory } from "react-router-dom";
+import styled from "styled-components"
+
+
+const Container=styled.div`
+display: flex;
+align-items: center;
+flex-direction: column;
+`
+
+const Form = styled.form`
+display: flex;
+flex-direction: column;
+width: 400px;
+input{
+    padding: 10px;
+    margin-bottom: 10px;
+}
+`
+
+
 
 const  ApplicationFormPage=()=>{
     const history = useHistory();
@@ -8,16 +28,35 @@ const  ApplicationFormPage=()=>{
         history.goBack("/");
     };
 
-
     return(
-        <>
-        <h1>Aplication home page</h1>
-        Para o usuário se candidatar à viagens, página que vai ter o formulário de inscrição
+        <Container>
+        <h1>Inscreverse para uma viagem</h1>
+
+       <Form>
+       <input
+       placeholder='escolher viagem'
+       />
+       <input
+       placeholder='Nome'
+       />
+       <input
+       placeholder='Idade'
+       />
+       <input
+       placeholder='texto de candidatura'
+       />
+       <input
+       placeholder='Profissão'
+       />
+       <input
+       placeholder='escolha um pais'
+       />
+       </Form>
        <button
        onClick={voltar}
        >voltar</button>
        <button>enviar</button>
-        </>
+        </Container>
         )
 }
 export default ApplicationFormPage
