49a034dbd198f0466c16a60c5f7d432116094795
matheus1810 <matheussouza1810@gmail.com>
2021-09-17 14:48:42 -0300

    falta só conseguir embaralhar as cartas

--- case/tarot/src/App.js
+++ case/tarot/src/App.js
@@ -6,20 +6,35 @@ import Card from "./compontents/Cards";
 
 
 const ContainerCards = styled.div`
+background-color: lightsteelblue;
 display: grid;
-grid-template-columns: repeat(12, 1fr);
+grid-template-columns: repeat(18,1fr);
 `
+const Header=styled.div`
+width: 100%;
+height: 50px;
+background-color: lightsteelblue;
+align-items: center;
+text-align: center;
+button{
+  background-color:lightcyan;
+  padding:10px;
+}
+`
+
 
 function App() {
 
-
-
   return (
+    <div>
+      <Header><button>iniciar jogo</button></Header>
     <ContainerCards >
+      
       {cards.cards.map((item,index)=>(
         <Card carta={item} key={`card-${index}`} />
       ))}
     </ContainerCards>
+    </div>
   );
 }
 
--- case/tarot/src/compontents/Cards.js
+++ case/tarot/src/compontents/Cards.js
@@ -1,28 +1,102 @@
 import React from "react";
 import ReactCardFlip from "react-card-flip";
-import cards from "../tarot.json"
+import { useState } from "react";
+import Modal from 'react-modal';
+import styled from "styled-components"
+
+
+const ImgContainer=styled.div`
+width:300px;
+height:350px;
+img{
+  width: 100%;
+  height: 100%;
+}
+
+`
+const ContainerModal=styled.div`
+display: flex;
+width:1000px;
+align-items: center;
+text-align: center;
+`
+const Description=styled.div`
+height: 100%;
+display: flex;
+justify-content: flex-end;
+`
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+const Img=styled.img`
+width:100px;
+height:180px;
+`
+
+
 
 
 
 const Card = ({ carta }) => {
-    const [isFlipped, setIsFlipped] = React.useState(false);
-    // console.log(project);
+  const [modalIsOpen, setIsOpen] =useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+    const [isFlipped, setIsFlipped] =useState(true)
+
+
     return (
-      <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+      <>
+
+<div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <ContainerModal onClick={closeModal}>
+       <ImgContainer>
+        <img src={`https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/162x341/${carta.image}`}/>
+       </ImgContainer>
+       <Description>
+        Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncov
+        er many web sites still in their infancy. Various versions have evolved over the ye
+        ars, sometimes by accident, sometimes on purpose (injected humour and the like
+       </Description>
+       </ContainerModal>
+      </Modal>
+    </div>
+       <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
         <div
          onClick={() => setIsFlipped((prev) => !prev)}
-          className="CardFront"
         >
-          <div>
-              <img src={`https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/162x341/${carta.image}`}/>
+          <div >
+              <Img src={`https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/162x341/${carta.image}`}/>
           </div>
 
-        </div>
+      </div>
         <div
           onClick={() => setIsFlipped((prev) => !prev)}>
-              <img src={"https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/back-blue-card.png"}/>
+              <Img onClick={openModal} src={"https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/back-blue-card.png"}/>
         </div>
       </ReactCardFlip>
+      </>
     );
   };
 
