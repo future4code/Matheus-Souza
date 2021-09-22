27c7c0451a5da83fa5f50d5a84ff834a52941907
matheus1810 <matheussouza1810@gmail.com>
2021-09-22 07:18:03 -0300

    criei um arquivo para cada exercício

--- semana15/semana15/introducao-typescript/src/exercicio1.ts
+++ semana15/semana15/introducao-typescript/src/exercicio1.ts
@@ -2,7 +2,7 @@
 //A)aparece um erro pois não aceito numeros em variaveis do tipo string
 //B)declarando number|string
 //C)
-/* type pessoa={
+type pessoa={
     nome:string,
     idade:number,
     corFavorita:string
@@ -39,30 +39,4 @@ const pessoa3:pessoa={
 
 const pessoas:pessoa[]=[pessoa1,pessoa2,pessoa3]
 
-console.table(pessoas) */
-
-//----------------------------------------Exercício 2 --------------------------------------------------------
-//A)
-
-
-function obterEstatisticas(numeros:number|any) {
-
-    const numerosOrdenados = numeros.sort(
-        (a:any, b:any) => a - b
-    )
-
-    let soma:number = 0
-
-    for (let num of numeros) {
-        soma += num
-    }
-
-    const estatisticas = {
-        maior: numerosOrdenados[numeros.length - 1],
-        menor: numerosOrdenados[0],
-        media: soma / numeros.length
-    }
-
-    return estatisticas
-}
-//B)não consegui essa
+console.table(pessoas) 
