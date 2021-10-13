b4888b38ca08d128f1c6d9169d26f1b1bb2357d4
matheus1810 <matheussouza1810@gmail.com>
2021-10-13 18:56:39 -0300

    entrega

--- semana18/heranaca-polimorfismo/src/index.ts
+++ semana18/heranaca-polimorfismo/src/index.ts
@@ -1,1 +1,9 @@
-console.log("oi")
+import { Customer } from "./classes/Customer"
+import {User} from "./classes/user"
+
+
+const usuario1=new User("1","matheus","matheus@email.dom","ashas")
+const usuarioCustumer=new Customer("2","marcos@gmail","marcos","132456","789789",)
+
+
+usuarioCustumer.introduceYourself()
