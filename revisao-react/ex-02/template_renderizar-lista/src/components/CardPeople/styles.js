bd44cc2d008bd340185c98776f67515b4e41a4aa
matheus1810 <matheussouza1810@gmail.com>
2022-02-03 16:11:02 -0300

    entrega ex2

---
+++ revisao-react/ex-02/template_renderizar-lista/src/components/CardPeople/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const CardPeopleContainer = styled.div `
+background-color: lightgray;
+height:250px;
+width:210px;
+border:1px solid black;
+`
+
+export const Container = styled.div`
+ display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr  ;
+  grid-column-gap:4px;
+  grid-template-rows:255px;
+`
---
+++ revisao-react/rev-react/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  
+  return (
+    <div >
+     HGome
+    </div>
+  )
+}
+
+export default App
